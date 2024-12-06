637e2b5444235b29baf4f7d07557b73a64048a0c
BasmalaAhmed <basmala.ahmed19.2004@gmail.com>
2024-12-07 00:59:08 +0200

    adding slotss

---
+++ Coworking_Space_System/src/users/info/system/LoginPage.java
@@ -0,0 +1,9 @@
+package users.info.system;
+
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+
+public class LoginPage implements ActionListener {
+    HashMap<String,String>logininfo=new HashMap<>();
+
+}
---
+++ Coworking_Space_System/src/users/info/system/Users.java
@@ -0,0 +1,16 @@
+package users.info.system;
+
+public abstract class Users {
+    protected String name;
+    private String password;
+    protected String email;
+    protected String PhoneNumber ;
+
+
+    public  void login(){
+
+    }
+    public void logout(){
+
+    }
+}
